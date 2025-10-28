50688098061d3bc4e8d4c1f6ba7a58c35d61ed2f
Angie <andatoba@espol.edu.ec>
2025-10-27 22:46:35 -0500

    Filtro de películas por género o nombre. Subida y visualización de imagen de portada de la película. Validaciones del lado del cliente y del servidor.

--- Models/Director.cs
+++ Models/Director.cs
@@ -6,9 +6,13 @@ namespace SitioWebDePeliculas.Models
     public class Director
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Nombre { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Nacionalidad { get; set; }
-
+        [Required]
         [Display(Name = "Fecha de Nacimiento")]
         [DataType(DataType.Date)]
         public DateTime FechaNacimiento { get; set; }
